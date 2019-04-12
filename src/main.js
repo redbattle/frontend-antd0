844516fb888e611c436ee7c1e5860ba179dc5785
zhaiyu <zhaiyu_hb@hotmail.com>
2019-04-12 12:18:24 +0800

    ant-design-vue

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Button from "ant-design-vue/lib/button";
+import 'ant-design-vue/dist/antd.css'
+
+Vue.config.productionTip = false
+
+Vue.component(Button.name, Button);
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
