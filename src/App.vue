fb765e50b1c54fb4217c412fac6f13bb1fa12001
zhaiyu <zhaiyu_hb@hotmail.com>
2019-04-17 17:56:21 +0800

    ant-design-vue

--- src/App.vue
+++ src/App.vue
@@ -2,54 +2,47 @@
   <a-layout id="components-layout-demo-fixed-sider" :style="{minHeight: '100%'}">
     <a-layout-sider :style="{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0 }">
       <div class="logo"></div>
-      <a-menu theme="dark" mode="inline" :defaultSelectedKeys="['4']">
-        <a-menu-item key="1">
-          <a-icon type="user" />
-          <span class="nav-text">nav 1</span>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <a-icon type="video-camera" />
-          <span class="nav-text">nav 2</span>
-        </a-menu-item>
-        <a-menu-item key="3">
-          <a-icon type="upload" />
-          <span class="nav-text">nav 3</span>
-        </a-menu-item>
-        <a-menu-item key="4">
-          <a-icon type="bar-chart" />
-          <span class="nav-text">nav 4</span>
-        </a-menu-item>
-        <a-menu-item key="5">
-          <a-icon type="cloud-o" />
-          <span class="nav-text">nav 5</span>
-        </a-menu-item>
-        <a-menu-item key="6">
-          <a-icon type="appstore-o" />
-          <span class="nav-text">nav 6</span>
-        </a-menu-item>
-        <a-menu-item key="7">
-          <a-icon type="team" />
-          <span class="nav-text">nav 7</span>
-        </a-menu-item>
-        <a-menu-item key="8">
-          <a-icon type="shop" />
-          <span class="nav-text">nav 8</span>
-        </a-menu-item>
+      <a-menu
+        theme="dark"
+        mode="inline"
+        :defaultSelectedKeys="['1']"
+        :defaultOpenKeys="['sub1']"
+      >
+        <a-sub-menu key="sub1">
+          <span slot="title"><a-icon type="user"/>subnav 1</span>
+          <a-menu-item key="1">option1</a-menu-item>
+          <a-menu-item key="2">option2</a-menu-item>
+          <a-menu-item key="3">option3</a-menu-item>
+          <a-menu-item key="4">option4</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <span slot="title"><a-icon type="laptop"/>subnav 2</span>
+          <a-menu-item key="5">option5</a-menu-item>
+          <a-menu-item key="6">option6</a-menu-item>
+          <a-menu-item key="7">option7</a-menu-item>
+          <a-menu-item key="8">option8</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub3">
+          <span slot="title"><a-icon type="notification"/>subnav 3</span>
+          <a-menu-item key="9">option9</a-menu-item>
+          <a-menu-item key="10">option10</a-menu-item>
+          <a-menu-item key="11">option11</a-menu-item>
+          <a-menu-item key="12">option12</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub4">
+          <span slot="title"><a-icon type="notification"/>subnav 3</span>
+          <a-menu-item key="13">option9</a-menu-item>
+          <a-menu-item key="14">option10</a-menu-item>
+          <a-menu-item key="15">option11</a-menu-item>
+          <a-menu-item key="16">option12</a-menu-item>
+        </a-sub-menu>
       </a-menu>
     </a-layout-sider>
     <a-layout :style="{ marginLeft: '200px', height: '100%'}">
-      <a-layout-header :style="{ background: '#fff', padding: 0 }" >diwehfdieuw</a-layout-header>
+      <a-layout-header :style="{ background: '#fff', padding: 0 }">diwehfdieuw</a-layout-header>
       <a-layout-content :style="{ margin: '24px 16px 0' }">
         <div :style="{ padding: '24px', background: '#fff', textAlign: 'center' }">
-          ...
-          <br />
-          Really
-          <br />...<br />...<br />...<br />
-          Really
-          <br />...<br />...<br />...<br />
-          Really
-          Really
-          long
+          <list></list>
         </div>
       </a-layout-content>
       <a-layout-footer :style="{ textAlign: 'center' }">
@@ -58,10 +51,22 @@
     </a-layout>
   </a-layout>
 </template>
+
+<script>
+  import Vue from 'vue'
+  import List from './components/List.vue'
+  Vue.use(List)
+  export default {
+    components: {
+      'list': List
+    }
+  }
+</script>
+
 <style>
   #components-layout-demo-fixed-sider .logo {
     height: 32px;
-    background: rgba(255,255,255,.2);
+    background: rgba(255, 255, 255, .2);
     margin: 16px;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { Layout, Menu, Breadcrumb, Icon } from 'ant-design-vue';
+import { Layout, Menu, Icon, Table, Popconfirm, Input } from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
 
-Vue.use(Layout, Menu, Breadcrumb, Icon);
+Vue.use(Layout)
+Vue.use(Menu)
+Vue.use(Icon)
+Vue.use(Table)
+Vue.use(Popconfirm)
+Vue.use(Input)
+
+Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App),
